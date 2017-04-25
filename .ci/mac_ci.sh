67eb5ad023742db05308b217a19e1c981def425d
turtled <turtled@emqtt.io>
2017-04-25 13:59:00 +0800

    update ci

--- .ci/mac_ci.sh
+++ .ci/mac_ci.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git clone -b v2.1.1 https://github.com/emqtt/emq-relx.git
+git clone -b release https://github.com/emqtt/emq-relx.git
 version=`cd emq-relx && git describe --abbrev=0 --tags`
 pkg=emqttd-macosx-${version}.zip
 echo "building $pkg..."
 cd emq-relx && make && cd _rel && zip -rq $pkg emqttd
+scp rel/emqttd.zip emqtt.io:/root/releases/$1
