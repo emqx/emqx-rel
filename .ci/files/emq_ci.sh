4636d8209aef396d0e3deace0276fe1e46715309
turtled <turtled@emqtt.io>
2017-04-25 10:56:58 +0800

    update ci

--- .ci/files/emq_ci.sh
+++ .ci/files/emq_ci.sh
@@ -5,7 +5,7 @@ git clone -b ${tag} https://github.com/emqtt/emq-relx.git
 version=`cd emq-relx && git describe --abbrev=0 --tags`
 pkg=emqttd-${ostype}-${version}.zip
 echo "building $pkg..."
-cd emq-relx && make && cd _rel && zip -rq $pkg emqttd && scp $pkg ubuntu@${host}:/home/ubuntu/package/ && cd /emq_temp
+cd emq-relx && make && cd _rel && zip -rq $pkg emqttd && scp $pkg root@${host}:/root/releases/${versionid} && cd /emq_temp
 
 git clone https://github.com/emqtt/emq-package.git
 cd emq-package
@@ -14,4 +14,4 @@ name=`basename package/*`
 name2=${name/emqttd-${versionid}/emqttd-${ostype}-${version}}
 name3=${name2/emqttd_${versionid}/emqttd-${ostype}-${version}}
 mv package/${name} package/${name3}
-scp package/* ubuntu@${host}:/home/ubuntu/package/
+scp package/* root@${host}:/root/releases/${versionid}
