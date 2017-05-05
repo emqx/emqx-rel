0ae3f707052f24622c0514e705751fe027e0205d
turtled <turtled@emqtt.io>
2017-05-05 17:27:04 +0800

    fixed conflicts

--- .ci/files/emq_ci.sh
+++ .ci/files/emq_ci.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 rm -rf /emq_temp && mkdir /emq_temp && cd /emq_temp
-git clone https://github.com/emqtt/emq-relx.git
+git clone -b ${tag} https://github.com/emqtt/emq-relx.git
 version=`cd emq-relx && git describe --abbrev=0 --tags`
 pkg=emqttd-${ostype}-${version}.zip
 echo "building $pkg..."
-cd emq-relx && make && cd _rel && zip -rq $pkg emqttd && scp $pkg ubuntu@${host}:/home/ubuntu/package/ && cd /emq_temp
+cd emq-relx && make && cd _rel && zip -rq $pkg emqttd && scp $pkg root@${host}:/root/releases/${versionid} && cd /emq_temp
 
 git clone https://github.com/emqtt/emq-package.git
 cd emq-package
 make
 name=`basename package/*`
-name2=${name/emqttd/emqttd-${ostype}}
-mv package/${name} package/${name2}
-scp package/* ubuntu@${host}:/home/ubuntu/package/
+name2=${name/emqttd-${versionid}/emqttd-${ostype}-${version}}
+name3=${name2/emqttd_${versionid}/emqttd-${ostype}-${version}}
+mv package/${name} package/${name3}
+scp package/* root@${host}:/root/releases/${versionid}
