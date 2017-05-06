26b25cf0ff6b054c5b9b5b027c102f2e4cc40862
turtled <turtled@emqtt.io>
2017-05-06 12:30:56 +0800

    Update ci

--- .ci/docker_ci.sh
+++ .ci/docker_ci.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Usage:
+#       ./docker_ci.sh {version}
+# Example:
+#       ./docker_ci.sh 2.2-beta.1
+
 package=emqttd-docker-v$1
 
 git clone https://github.com/emqtt/emq-docker
+
+cd emq-docker
 
 sudo docker build -t ${package} .
 
