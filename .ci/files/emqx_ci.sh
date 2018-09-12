5c65de5553910a116aaeb58744ae5a27e2eb12ad
chongyuan <yinchongyuan@xmeter.net>
2018-09-12 09:07:17 +0800

    Update emqx_ci.sh to fit Jenkins build

--- .ci/files/emqx_ci.sh
+++ .ci/files/emqx_ci.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
 chmod 600 /root/.ssh/config
-rm -rf /emqx_temp && mkdir /emqx_temp && cd /emqx_temp
-git clone -b emqx30_release https://github.com/emqx/emqx-rel
-version=`cd emqx-rel && git describe --abbrev=0 --tags`
-pkg=emqx-${ostype}-${version}.zip
-echo "building $pkg..."
-cd emqx-rel && make && cd _rel && zip -rq $pkg emqx && scp $pkg root@${host}:/root/releases/${versionid}-${type} && cd /emqx_temp
 
-git clone -b emqx30 https://github.com/emqx/emqx-packages
-cd emqx-packages
-make
-name=`basename package/*`
-name2=${name/emqx-${versionid}/emqx-${ostype}-${version}}
-name3=${name2/emqx_${versionid}/emqx-${ostype}-${version}}
-mv package/${name} package/${name3}
-scp package/* root@${host}:/root/releases/${versionid}-${type}
+if [[ ${tag} =~ "-ci" ]]
+then
+
+  today=$(date +%Y%m%d)
+  ssh ubuntu@${host}  "mkdir /home/ubuntu/releases/${versionid}-${type}"
+  ssh ubuntu@${host}  "mkdir /home/ubuntu/releases/${versionid}-${type}/${today}"
+
+  rm -rf /emqx_temp && mkdir /emqx_temp
+  cp -rf /emqx_code/* /emqx_temp/
+  cd /emqx_temp
+  pkg=emqx-${ostype}-${versionid}-${type}-${today}.zip
+  echo "building $pkg..."
+  cd emqx-rel && make && cd _rel && zip -rq $pkg emqx && scp $pkg ubuntu@${host}:/home/ubuntu/releases/${versionid}-${type}/${today} && cd /emqx_temp
+
+  cd emqx-packages
+  make
+  name=`basename package/*`
+  name2=${name/emqx-${versionid}/emqx-${ostype}-${versionid}-${type}-${today}}
+  name3=${name2/emqx_${versionid}/emqx-${ostype}-${versionid}-${type}-${today}}
+  mv package/${name} package/${name3}
+  scp package/* ubuntu@${host}:/home/ubuntu/releases/${versionid}-${type}/${today}
+
+else
+  rm -rf /emqx_temp && mkdir /emqx_temp && cd /emqx_temp
+  git clone -b emqx30_release https://github.com/emqx/emqx-rel
+  version=`cd emqx-rel && git describe --abbrev=0 --tags`
+  pkg=emqx-${ostype}-${version}.zip
+  echo "building $pkg..."
+  cd emqx-rel && make && cd _rel && zip -rq $pkg emqx && scp $pkg root@${host}:/root/releases/${versionid}-${type} && cd /emqx_temp
+
+  git clone -b emqx30 https://github.com/emqx/emqx-packages
+  cd emqx-packages
+  make
+  name=`basename package/*`
+  name2=${name/emqx-${versionid}/emqx-${ostype}-${version}}
+  name3=${name2/emqx_${versionid}/emqx-${ostype}-${version}}
+  mv package/${name} package/${name3}
+  scp package/* root@${host}:/root/releases/${versionid}-${type}
+fi
