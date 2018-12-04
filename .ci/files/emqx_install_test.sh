e6b5cf0f621287322ab5828ea57e9095e8b2a60d
Gilbert <gilbertw@emqx.io>
2018-12-04 10:48:56 +0800

    Revert "Add check package version"
    This reverts commit 2ef7b6a407d11252291b72c4a268705657eb740a.

--- .ci/files/emqx_install_test.sh
+++ .ci/files/emqx_install_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-function running_emqx(){
-    if [[ ! -z $(echo $version | grep -oE "v[0-9]+\.[0-9]+(\.[0-9]+)?-(alpha|beta|rc)\.[0-9]") ]]
+packagespath=${buildlocation}
+
+if [[ $ostype == centos* ]]
+then
+    packagename=`basename $packagespath/emqx-$ostype-$version*.rpm`
+    rpm -ivh $packagespath/$packagename
+    if [[ $(rpm -q emqx) != emqx* ]]
     then
-        if [[ -z $(ls /usr/lib/emqx/lib| grep emqx-$version) ]]
-        then
-            echo "emqx package version error"
-            exit 1
-        fi
+        echo "package install error"
+    exit 1
     fi
 
     emqx start
@@ -27,21 +29,6 @@ function running_emqx(){
         fi
         service emqx stop
     fi
-}
-
-packagespath=${buildlocation}
-
-if [[ $ostype == centos* ]]
-then
-    packagename=`basename $packagespath/emqx-$ostype-$version*.rpm`
-    rpm -ivh $packagespath/$packagename
-    if [[ $(rpm -q emqx) != emqx* ]]
-    then
-        echo "package install error"
-    exit 1
-    fi
-
-    running_emqx
 
     rpm -e emqx
     if [[ $(rpm -q emqx) == emqx* ]]
@@ -58,7 +45,21 @@ else
         exit 1
     fi
 
-    running_emqx
+    emqx start
+    if [[ -z "$(emqx_ctl status |grep 'is running'|awk '{print $1}')" ]]
+    then
+        echo "emqx running error"
+        exit 1
+    fi
+    emqx stop
+
+    service emqx start
+    if [[ -z "$(emqx_ctl status |grep 'is running'|awk '{print $1}')" ]]
+    then
+        echo "emqx service error"
+        exit 1
+    fi
+    service emqx stop
 
     dpkg -r emqx
     if [ $(dpkg -l |grep emqx |awk '{print $1}') != "rc" ]
