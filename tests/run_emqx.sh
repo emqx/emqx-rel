33278d590ef181ea8992373dc2dc4f209c3270ac
Gilbert Wong <gilbertwong96@icloud.com>
2018-12-27 10:45:19 +0800

    Fix the problem run in docker

--- tests/run_emqx.sh
+++ tests/run_emqx.sh
@@ -4,5 +4,7 @@ make -C .. deps
 
 while IFS='' read line || [[ -n $line ]]; do
     echo ============start test $line===============
+    rm -rf ../deps/$line/deps
+    rm -rf ../deps/$line/erlang.mk
     make -C ../deps/$line/ tests </dev/null
 done < $1
