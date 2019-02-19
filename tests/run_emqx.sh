c24c77dfc9b59e1a8c98b6ad0ee24950f75fd36b
turtled <turtled@emqtt.io>
2019-02-19 10:20:36 +0800

    Update run_emqx.sh

--- tests/run_emqx.sh
+++ tests/run_emqx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-EMQX_DEPS_DEFAULT_VSN=${EMQX_DEPS_DEFAULT_VSN:-emqx30}
+
+set -x
+
+EMQX_DEPS_DEFAULT_VSN=${EMQX_DEPS_DEFAULT_VSN:-testing}
 git clone -b $EMQX_DEPS_DEFAULT_VSN https://github.com/emqx/emqx-auth-username emqx_auth_username 
 cd emqx_auth_username && make
 cd ..
