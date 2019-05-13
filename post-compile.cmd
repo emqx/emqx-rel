4647fc7197c5aa22cb36278c85c97e63e6c0d761
Gilbert Wong <gilbertwong96@icloud.com>
2019-05-13 19:29:21 +0800

    Finish post-compile.cmd for windows

--- post-compile.cmd
+++ post-compile.cmd
@@ -1,24 +1,51 @@
+@echo off
+:: cd %REBAR_BUILD_DIR%
 
-cd %REBAR_BUILD_DIR%
-
+:: echo %REBAR_BUILD_DIR%
 :: Collect config files, some are direct usable
 :: some are relx-overlay templated
-rmdir /s/q conf
-mkdir conf\plugins
-@for %%Conf in ("lib\*\etc\*.conf*") do @(
-     if "emqx.conf" == %%~nConf
-         copy %%Conf conf\
-     else ^
-     if "acl.conf" == %%~nConf
-         copy %%Conf conf\
-     else ^
-     if "ssl_dist.conf" == %%~nConf
-         copy %%Conf conf\
-     else ^
-     copy %%Conf conf\plugins\
-)
+:: pushd _build\emqx\
+:: cd %REBAR_BUILD_DIR%
 
-mkdir conf/schema
-@for %%schema in ("lib\*\priv\*.schema") do @(
-    copy %%schema conf\schema\
+:: echo %REBAR_BUILD_DIR%
+:: Collect config files, some are direct usable
+:: some are relx-overlay templated
+:: pushd _build\emqx\
+
+:: set REBAR_BUILD_DIR=_build\emqx
+pushd %REBAR_BUILD_DIR%
+
+rmdir /s/q %REBAR_BUILD_DIR%\conf
+mkdir %REBAR_BUILD_DIR%\conf\plugins
+mkdir %REBAR_BUILD_DIR%\conf\schema
+set ConfPath=%CD%
+
+for /d %%i in ("lib\emqx*") do call :conf %%i
+
+for /d %%i in ("lib\emqx*") do call :schema %%i
+
+popd
+
+:conf
+for %%f in ("%1\etc\*.conf") do (
+    :: echo %%f
+    if "emqx" == "%%~nf" (
+        copy %%f %REBAR_BUILD_DIR%\conf\
+    ) else (
+        if "acl" == "%%~nf" (
+            copy %%f %REBAR_BUILD_DIR%\conf\
+        ) else ( 
+            if "ssl_dist" == "%%~nf" (
+                copy %%f %REBAR_BUILD_DIR%\conf\
+            ) else copy %%f %REBAR_BUILD_DIR%\conf\plugins
+        )
+    )
 )
+:end
+
+:schema
+for %%f in ("%1\priv\*.schema") do (
+    ::echo %%f
+    copy %%f %REBAR_BUILD_DIR%\conf\schema\
+)
+:end
