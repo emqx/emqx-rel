eeed717ce52cb5ef142aa5178ef66207e21d1ed8
Gilbert Wong <gilbertwong96@icloud.com>
2019-05-14 11:27:29 +0800

    Update post-compile.cmd

--- post-compile.cmd
+++ post-compile.cmd
@@ -12,40 +12,43 @@
 :: some are relx-overlay templated
 :: pushd _build\emqx\
 
-:: set REBAR_BUILD_DIR=_build\emqx
-pushd %REBAR_BUILD_DIR%
+set REBAR_BUILD_DIR=_build\emqx
 
 rmdir /s/q %REBAR_BUILD_DIR%\conf
 mkdir %REBAR_BUILD_DIR%\conf\plugins
 mkdir %REBAR_BUILD_DIR%\conf\schema
-set ConfPath=%CD%
 
+pushd %REBAR_BUILD_DIR%
+
+set ConfPath=%CD%
 for /d %%i in ("lib\emqx*") do call :conf %%i
 
 for /d %%i in ("lib\emqx*") do call :schema %%i
 
-popd
-
 :conf
-for %%f in ("%1\etc\*.conf") do (
+pushd %1
+for %%f in ("etc\*.conf") do (
     :: echo %%f
     if "emqx" == "%%~nf" (
-        copy %%f %REBAR_BUILD_DIR%\conf\
+        copy %%f %ConfPath%\conf\
     ) else (
         if "acl" == "%%~nf" (
-            copy %%f %REBAR_BUILD_DIR%\conf\
+            copy %%f %ConfPath%\conf\
         ) else ( 
             if "ssl_dist" == "%%~nf" (
-                copy %%f %REBAR_BUILD_DIR%\conf\
-            ) else copy %%f %REBAR_BUILD_DIR%\conf\plugins
+                copy %%f %ConfPath%\conf\
+            ) else copy %%f %ConfPath%\conf\plugins
         )
     )
 )
+popd
 :end
 
 :schema
-for %%f in ("%1\priv\*.schema") do (
+pushd %1
+for %%f in ("priv\*.schema") do (
     ::echo %%f
-    copy %%f %REBAR_BUILD_DIR%\conf\schema\
+    copy %%f %ConfPath%\conf\schema\
 )
+popd
 :end
