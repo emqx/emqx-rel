081293a69454f4add4589dee73f42a8065741e96
Gilbert <gilbertwong96@icloud.com>
2019-06-28 19:22:07 +0800

    Fix the race condition error `erlsrv.exe: unrecognize option` (#381)
    * Fix the race condition error `erlsrv.exe: unrecognize option`

--- bin/emqx.cmd
+++ bin/emqx.cmd
@@ -176,15 +176,15 @@
 :: Install the release as a Windows service
 :: or install the specified version passed as argument
 :install
+@call :create_mnesia_dir
+@call :generate_app_config
+:: Install the service
+@set args="-boot %boot_script% %sys_config% %generated_config_args% -mnesia dir '%mnesia_dir%'"
+@set description=EMQ node %node_name% in %rootdir%
 @if "" == "%2" (
-  call :create_mnesia_dir
-  call :generate_app_config
-  :: Install the service
-  set args="-boot %boot_script% %sys_config% %generated_config_args% -mnesia dir '%mnesia_dir%'"
-  set description=EMQ node %node_name% in %rootdir%
   %erlsrv% add %service_name% %node_type% "%node_name%" -on restart -d new -c "%description%" ^
            -w "%rootdir%" -m %erl_exe% -args %args% ^
-           -stopaction "init:stop()."
+           -st "init:stop()."
 ) else (
   :: relup and reldown
   goto relup
