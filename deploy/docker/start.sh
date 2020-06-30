e1428aae5a15817fcd0bba251c1985cf6e93c3b2
zhanghongtong <rory-z@outlook.com>
2020-07-01 00:18:16 +0800

    fix(docker): update start.sh

--- deploy/docker/start.sh
+++ deploy/docker/start.sh
@@ -2,9 +2,13 @@
 set -e -u
 
 emqx_exit(){
-    # tail emqx.log.*
-    num=$(sed -n -e '/LOGGING STARTED/=' /opt/emqx/log/emqx.log | tail -1)
-    tail -n +$((num-2)) /opt/emqx/log/emqx.log
+    # At least erlang.log.1 exists
+    if [ -f /opt/emqx/log/erlang.log.1 ]; then
+        # tail emqx.log.*
+        erlang_log=$(echo $(ls -t /opt/emqx/log/erlang.log.*) | awk '{print $1}')
+        num=$(sed -n -e '/LOGGING STARTED/=' ${erlang_log} | tail -1)
+        [ ! -z $num ] && [ $num -gt 2 ] && tail -n +$(expr $num - 2) ${erlang_log}
+    fi
 
     echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqx exit abnormally"
     exit 1
@@ -12,11 +16,14 @@ emqx_exit(){
 
 ## EMQ Main script
 
+# When receiving the EXIT signal, execute emqx_exit function
+trap "emqx_exit" EXIT
+
 # Set log.rotation=off in etc/emqx.conf
 sed -i -r 's/log.rotation = (.*)/log.rotation = off/1' /opt/emqx/etc/emqx.conf
 
 # Start and run emqx, and when emqx crashed, this container will stop
-/opt/emqx/bin/emqx start || emqx_exit
+/opt/emqx/bin/emqx start
 
 # Sleep 5 seconds to wait for the loaded plugins catch up.
 sleep 5
@@ -32,7 +39,7 @@ tail -f /opt/emqx/log/emqx.log &
 IDLE_TIME=0
 MGMT_CONF='/opt/emqx/etc/plugins/emqx_management.conf'
 MGMT_PORT=$(sed -n -r '/^management.listener.http[ \t]=[ \t].*$/p' $MGMT_CONF | sed -r 's/^management.listener.http = (.*)$/\1/g')
-while [[ $IDLE_TIME -lt 5 ]]; do
+while [ $IDLE_TIME -lt 5 ]; do
     IDLE_TIME=$(expr $IDLE_TIME + 1)
     if curl http://localhost:${MGMT_PORT}/status >/dev/null 2>&1; then
         IDLE_TIME=0
@@ -43,5 +50,4 @@ while [[ $IDLE_TIME -lt 5 ]]; do
 done
 # If running to here (the result 5 times not is running, thus in 25s emqx is not running), exit docker image
 # Then the high level PaaS, e.g. docker swarm mode, will know and alert, rebanlance this service
-
-emqx_exit
+exit 1
