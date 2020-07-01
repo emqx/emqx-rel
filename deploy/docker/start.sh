8dafe945540ff4839875794343ba676a095ffde5
zhanghongtong <rory-z@outlook.com>
2020-07-01 09:23:08 +0800

    fix(docker): update start.sh

--- deploy/docker/start.sh
+++ deploy/docker/start.sh
@@ -30,6 +30,9 @@ sleep 5
 
 echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqx start"
 
+## Fork tailing emqx.log, the fork is not killed after this script exits
+## The assumption is that this is the docker entrypoint,
+## hence docker container is terminated after entrypoint exists
 tail -f /opt/emqx/log/emqx.log &
 
 # monitor emqx is running, or the docker must stop to let docker PaaS know
@@ -48,6 +51,6 @@ while [ $IDLE_TIME -lt 5 ]; do
     fi
     sleep 5
 done
+
 # If running to here (the result 5 times not is running, thus in 25s emqx is not running), exit docker image
 # Then the high level PaaS, e.g. docker swarm mode, will know and alert, rebanlance this service
-exit 1
