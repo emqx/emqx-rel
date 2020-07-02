846977223c2915e95d0a0538945c5fdb0ada8187
zhanghongtong <rory-z@outlook.com>
2020-07-02 10:55:51 +0800

    chore(docker): loaded_modules and loaded_plugins backward compatible

--- deploy/docker/docker-entrypoint.sh
+++ deploy/docker/docker-entrypoint.sh
@@ -162,6 +162,8 @@ if [[ ! -z "$EMQX_LOADED_PLUGINS" ]]; then
     for var in $(echo "$EMQX_LOADED_PLUGINS"|sed -e "s/^[^A-Za-z0-9_]\{1,\}//g"|sed -e "s/[^A-Za-z0-9_]\{1,\}/\ /g"); do
         if [ ! -z $(grep -o $var ${_EMQX_HOME}/data/loaded_plugins) ]; then
             echo "$(sed -r "s/\{($var),[ ]*(true|false)\}./\{\1, true\}./1" ${_EMQX_HOME}/data/loaded_plugins)" > ${_EMQX_HOME}/data/loaded_plugins
+            # backward compatible.
+            echo "$(sed -r -e "s/($var)./\{\1, true\}./1" ${_EMQX_HOME}/data/loaded_plugins)" > ${_EMQX_HOME}/data/loaded_plugins
         else
             echo "{$var, true}." >> ${_EMQX_HOME}/data/loaded_plugins
         fi
@@ -177,6 +179,8 @@ if [[ ! -z "$EMQX_LOADED_MODULES" ]]; then
     for var in $(echo "$EMQX_LOADED_MODULES"|sed -e "s/^[^A-Za-z0-9_]\{1,\}//g"|sed -e "s/[^A-Za-z0-9_]\{1,\}/\ /g"); do
         if [ ! -z $(grep -o $var ${_EMQX_HOME}/data/loaded_modules) ]; then
             echo "$(sed -r "s/\{($var),[ ]*(true|false)\}./\{\1, true\}./1" ${_EMQX_HOME}/data/loaded_modules)" > ${_EMQX_HOME}/data/loaded_modules
+            # backward compatible.
+            echo "$(sed -r -e "s/($var)./\{\1, true\}./1" ${_EMQX_HOME}/data/loaded_modules)" > ${_EMQX_HOME}/data/loaded_modules
         else
             echo "{$var, true}." >> ${_EMQX_HOME}/data/loaded_modules
         fi
